b27c102217973331e84c6a1162da2b66708b7082
Marissa Baron <mariluvv23@gmail.com>
2023-02-01 13:55:25 -0500

    created bestsellers page

---
+++ client/src/pages/BestSellers.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import {
+  Modal,
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  CardColumns,
+  InputGroup,
+} from "react-bootstrap";
+
+import { useMutation } from "@apollo/client";
