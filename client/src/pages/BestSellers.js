f15a52a3c43e4ea47ef50012c6db3ee5fc03e4a9
Marissa Baron <mariluvv23@gmail.com>
2023-02-15 19:19:35 -0500

    cleaned code in nyt page

--- client/src/pages/BestSellers.js
+++ client/src/pages/BestSellers.js
@@ -4,21 +4,13 @@ import { Modal, Container, Col, Button, Card, Row } from "react-bootstrap";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
-import { SAVE_BOOK } from "../utils/mutations";
-import Auth from "../utils/auth";
-import { useMutation } from "@apollo/client";
 
-import { faSquarePlus } from "@fortawesome/free-solid-svg-icons";
-import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
+
+
 const info = <FontAwesomeIcon icon={faCircleInfo} size="2xl" color="" />;
-const addIcon = <FontAwesomeIcon icon={faSquarePlus} size='2xl'/>;
-const savedIcon = <FontAwesomeIcon icon={faSquareCheck} size='2xl'/>
 const apiKey = "odAC47mAXREvZ3HvHdv5XieoP4WcAzVm";
 
 const BestSellers = () => {
-  const [savedBookIds, setSavedBookIds] = useState([]);
-
-  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
 
   const [lists, setLists] = useState([]);
@@ -26,46 +18,22 @@ const BestSellers = () => {
 
   const handleClose = () => setShow(false);
   const handleShow = (id) => setShow(id);
-  //   const [books, setListBooks] = useState([])
+    // const [books, setListBooks] = useState([])
   
   useEffect(() => {
     const fetchBooks = async () => {
       const res = await axios.get(
         `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${apiKey}`
       );
-      const bookData = lists.map((books) =>({
-      bookId:books.books[0].primary_isbn10 
-    }));
-      setLists(res.data.results.lists, bookData);
-      
-      //   setListBooks(res.data.results.lists.books)
-      console.log(res.data.results.lists, bookData);
+      setLists(res.data.results.lists);
+      console.log(res.data.results.lists);
       
     };
 
     fetchBooks();
   }, []);
 
-  const handleSaveBook = async (bookId) => {
-     
-    const bookToSave = lists.find((books) => books.bookId===bookId);
-  
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const { data } = await saveBook({
-        variables: { bookData: { ...bookToSave } },
-      });
-      console.log(savedBookIds);
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
-    }
-  };
 
   return (
     <>
@@ -83,7 +51,7 @@ const BestSellers = () => {
                   {list_name}
                 </h2>
                 {books.map((book) => {
-                  const { book_image, author, title, description, primary_isbn10 } = book;
+                  const { book_image, author, title, description } = book;
     
                   return (
                     <Col
@@ -131,24 +99,9 @@ const BestSellers = () => {
                               <Button variant="secondary" onClick={handleClose}>
                                 Close
                               </Button>
-
-                              
                             </Modal.Footer>
                           </Modal>
-                          {Auth.loggedIn() && (
-                    <Button
-                      disabled={savedBookIds?.some(
-                        (savedId) => savedId === books.bookId
-                      )}
-                      className="btn-light"
-                      style={{backgroundColor:'white'}}
-                      onClick={() => handleSaveBook(books.bookId)}
-                    >
-                      {savedBookIds?.some((savedId) => savedId === books.bookId)
-                        ? savedIcon
-                        : addIcon}
-                    </Button>
-                  )}
+                          
                         </Card.Footer>
                       </Card>
                     </Col>
